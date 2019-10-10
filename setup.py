7243a223c842e1a4a4c4c0d73606e3dc973c670a
Vin985 <sylvain.christin@gmail.com>
2019-10-10 12:24:13 -0300

    refactor and compatibility changes

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "sound4python",
-    version = "0.1.2",
-    author = "Peter Rennert, dave.crist@gmail.com",
-    author_email = "p.rennert@cs.ucl.ac.uk, dave.crist@gmail.com",
-    description = ("comfortable playback of wav files"),
+    name="sound4python",
+    version="0.2.0",
+    author="Peter Rennert, dave.crist@gmail.com, vin985",
+    author_email="p.rennert@cs.ucl.ac.uk, dave.crist@gmail.com",
+    description=("comfortable playback of wav files"),
     packages=find_packages(),
-    license = read('LICENSE.txt'),
-    keywords = "wav",
-    url = "https://github.com/groakat/sound4python",
+    license=read('LICENSE.txt'),
+    keywords="wav",
+    url="https://github.com/vin985/sound4python",
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 4 - Beta",
